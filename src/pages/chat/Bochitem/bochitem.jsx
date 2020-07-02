d52643ca7fb170332565c7472da97e85993e4e25
Poleself <poleselfg@gmail.com>
2020-07-02 19:30:12 +0200

    ultimo del dia

---
+++ src/pages/chat/Bochitem/bochitem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./bochitem.css";
+
+const Bochitem = () => {
+  return <div>bochitem</div>;
+};
+
+export default Bochitem;
--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./chat.css";
+import Bochitem from "./Bochitem/bochitem";
 
 const Chat = () => {
-  return <div>soy el chat</div>;
+  return (
+    <div className="chatbot-chat-container">
+      <div className="chatbot-chat-content">
+        <div className="chatbot-chat">
+          <div className="chatbot-chat-container-body">
+            <Bochitem />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
